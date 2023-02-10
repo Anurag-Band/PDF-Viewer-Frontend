1966b6871ff6bb8201e0a15d57e6caa97c35a76b
Anurag-Band <anuragband10@gmail.com>
2023-02-10 14:51:44 +0530

    added correct backend url in axios base url

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "https://pdf-viewer-backend-ibdl.onrender.com",
+  baseURL: "https://pdf-viewer-backend-ibdl.onrender.com/api/v1",
   timeout: 8000,
   headers: {
     Accept: "application/json",
